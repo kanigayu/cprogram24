7211e3940e821fab711268935d57adf90d7e62e4
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-02-05 16:55:49 +0530

    small.c
    small

---
+++ small.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i,k;
+printf("enter the num of array\n");
+scanf("%d",&size);
+for(i=0;i<k;++i)
+{
+	printf("enter the elements\n",i+1);
+	scanf("%d",&a[i]);
+}
+
+for(i=1;i<k;++i)
+{
+	if(a[0]>a[i])
+	a[0]=a[i];
+}
+printf("largest element%d\n",a[0]);
+return 0;
+}
